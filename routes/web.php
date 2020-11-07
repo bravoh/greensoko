59058d1fe9cc7486b2026aee8af8aca1ce9cc250
Bravo Gidi <kiptoobravo@gmail.com>
2020-11-07 11:56:32 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-
+Route::post('/deploy', 'DeployController@deploy')->name('deploy')->middleware('auth');
 Route::post('/post', 'PostController@index')->name('post')->middleware('auth');
 Route::get('/post', 'PostController@index')->name('post')->middleware('auth');
 Route::get('/my-posts', 'PostController@myposts')->name('my-posts')->middleware('auth');
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['auth'],], function () {
     Route::get('/', 'AdminController@index')->name('admin');
-
     Route::get('category', 'HomeController@category')->name('category');
     Route::post('category', 'AdminController@category')->name('category');
 });
